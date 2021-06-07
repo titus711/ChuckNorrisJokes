ea055e2127665352111339fcfdc5149eb216c60e
Tito Nangi <titusnangi@gmail.com>
2021-06-07 15:01:03 +0200

    base_url declaration

--- app/src/main/java/com/titusnangi/chucknorrisjokes/utils/Credentials.java
+++ app/src/main/java/com/titusnangi/chucknorrisjokes/utils/Credentials.java
@@ -1,4 +1,10 @@
 package com.titusnangi.chucknorrisjokes.utils;
 
 public class Credentials {
+
+    // declaring the base_url of the api as a constant
+
+    public static final String BASE_URL = "https://api.chucknorris.io";
+
+
 }
