32c63737cd39d5ec41a5b503ad4d015696cdfc99
Tito Nangi <titusnangi@gmail.com>
2021-06-09 01:13:18 +0200

    Testing the search response in the JokeListActivity, and making multiple code changes in the JokeModel,Result,JokeApi and Service classes

--- app/src/main/java/com/titusnangi/chucknorrisjokes/models/JokeModel.java
+++ app/src/main/java/com/titusnangi/chucknorrisjokes/models/JokeModel.java
@@ -1,69 +1,38 @@
 package com.titusnangi.chucknorrisjokes.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class JokeModel implements Parcelable {
-
-    //implementing the Parcelable interface allows data to be passed
-    // from one activity to another
 
 
-    private String joke_value;
-    private String url;
-    private String joke_id;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+//this is the root model class that is mapping the JSON objects into Java objects
+public class JokeModel {
 
 
-    //constructor
-    public JokeModel(String joke_value, String url, String joke_id) {
-        this.joke_value = joke_value;
-        this.url = url;
-        this.joke_id = joke_id;
+
+    @SerializedName("total")
+    @Expose
+    private int total;
+
+    @SerializedName("result")
+    @Expose
+    private List<Result> mResultList;
+
+    public int getTotal() {
+        return total;
     }
 
-
-
-
-    protected JokeModel(Parcel in) {
-        joke_value = in.readString();
-        url = in.readString();
-        joke_id = in.readString();
+    public void setTotal(int total) {
+        this.total = total;
     }
 
-    public static final Creator<JokeModel> CREATOR = new Creator<JokeModel>() {
-        @Override
-        public JokeModel createFromParcel(Parcel in) {
-            return new JokeModel(in);
-        }
-
-        @Override
-        public JokeModel[] newArray(int size) {
-            return new JokeModel[size];
-        }
-    };
-
-    //getters
-    public String getJoke_value() {
-        return joke_value;
+    public List<Result> getResultList() {
+        return mResultList;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public String getJoke_id() {
-        return joke_id;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(joke_value);
-        parcel.writeString(url);
-        parcel.writeString(joke_id);
+    public void setResultList(List<Result> resultList) {
+        mResultList = resultList;
     }
 }
