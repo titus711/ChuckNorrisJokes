2172bfea2ad30943ef84cda71359b0dc3a8269a3
Tito Nangi <titusnangi@gmail.com>
2021-06-09 08:01:51 +0200

    Update JokeListViewModel class
    Update JokeListViewModel class

--- app/src/main/java/com/titusnangi/chucknorrisjokes/viewmodels/JokeListViewModel.java
+++ app/src/main/java/com/titusnangi/chucknorrisjokes/viewmodels/JokeListViewModel.java
@@ -4,23 +4,29 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.titusnangi.chucknorrisjokes.models.JokeModel;
+import com.titusnangi.chucknorrisjokes.repositories.JokeRepository;
 
 import java.util.List;
 
 public class JokeListViewModel extends ViewModel {
-    //this class is  used as the VIEWMODEL
+    //this class is  used as the viewmodel
 
-    // making an instance of the LiveData
-    private MutableLiveData<List<JokeModel>> mJokes = new MutableLiveData<>();
+
+    //creating an instance of the repository
+
+    private JokeRepository jokeRepository;
+
 
     //constructor
+
     public JokeListViewModel() {
+        jokeRepository = JokeRepository.getInstance();
 
     }
 
     //getter
     public MutableLiveData<List<JokeModel>> getJokes() {
-        return mJokes;
+        return jokeRepository.getJokes();
     }
 
 
