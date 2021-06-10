296ff473ca59387f242d40c075830e1ae61b08a3
Tito Nangi <titusnangi@gmail.com>
2021-06-10 02:30:10 +0200

    creating the joke_list_item layout

--- app/src/main/java/com/titusnangi/chucknorrisjokes/JokeListActivity.java
+++ app/src/main/java/com/titusnangi/chucknorrisjokes/JokeListActivity.java
@@ -49,7 +49,7 @@ public class JokeListActivity extends AppCompatActivity {
         });
 
 
-        // creating an instance of the viewmodel
+        // creating an instance of the view-model
         jokeListViewModel = new ViewModelProvider(this).get(JokeListViewModel.class);
 
         // calling the observers
@@ -65,8 +65,11 @@ public class JokeListActivity extends AppCompatActivity {
             public void onChanged(List<JokeModel> jokeModels) {
                 if (jokeModels != null){
                     for (JokeModel jokeModel: jokeModels){
+
                         //Get the data in log
-                        Log.v("Tag", "onChanged: " + jokeModel.getValue());
+                        Log.v("Tag", "onChanged: " + jokeModel.getId());
+
+
                     }
                 }
 
@@ -81,47 +84,6 @@ public class JokeListActivity extends AppCompatActivity {
     }
 
 
-//    private void getRetrofitResponse() {
-//
-//        JokeApi jokeApi = Service.getJokeApi();
-//
-//        Call<JokeSearchResponse> responseCall = jokeApi
-//                .searchMovie("Chuck Norris");
-//
-//        responseCall.enqueue(new Callback<JokeSearchResponse>() {
-//            @Override
-//            public void onResponse(Call<JokeSearchResponse> call, Response<JokeSearchResponse> response) {
-//                if (response.code() == 200){
-//                    Log.v("Tag","the response" + response.body().toString());
-//
-//                    List<JokeModel> jokes = new ArrayList<>(response.body().getJokes());
-//
-//                    for (JokeModel joke: jokes ){
-//                        Log.v("Tag" , "The id " + joke.getId());
-//                        Log.v("Tag" , "The value " + joke.getValue());
-//                        Log.v("Tag" , "The url " + joke.getUrl());
-//
-//
-//                    }
-//                }
-//
-//                else {
-//                    try {
-//                        Log.v("Tag", "Error" + response.errorBody().string());
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//
-//
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<JokeSearchResponse> call, Throwable t) {
-//
-//            }
-//        });
-//    }
+
 
 }
