f046e6e3c6a3eef4cbf86a069bdcd7331ef1889e
Tito Nangi <titusnangi@gmail.com>
2021-06-10 03:03:02 +0200

    creating the adapters and  the joke_list_item layout
    creating the adapters and  the joke_list_item layout

---
+++ app/src/main/java/com/titusnangi/chucknorrisjokes/adapters/JokeViewHolder.java
@@ -0,0 +1,17 @@
+package com.titusnangi.chucknorrisjokes.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class JokeViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public JokeViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
